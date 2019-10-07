0db38c2bb9ba1dd626be3dc96400ce181dec5f6f
Meng Meng <849742405@qq.com>
2019-10-07 23:36:42 +0800

    first commit

---
+++ section_3/backend/vertex.cc
@@ -0,0 +1,30 @@
+#include "backend/vertex.h"
+#include <iostream>
+
+namespace myslam {
+namespace backend {
+
+unsigned long global_vertex_id = 0;//全局变量存储vertex，记录vectex的id索引
+
+Vertex::Vertex(int num_dimension, int local_dimension) {
+    parameters_.resize(num_dimension, 1);
+    local_dimension_ = local_dimension > 0 ? local_dimension : num_dimension;
+    id_ = global_vertex_id++;
+}
+
+Vertex::~Vertex() {}
+
+int Vertex::Dimension() const {
+    return parameters_.rows();
+}
+
+int Vertex::LocalDimension() const {
+    return local_dimension_;
+}
+
+void Vertex::Plus(const VecX &delta) {
+    parameters_ += delta;
+}
+
+}
+}
